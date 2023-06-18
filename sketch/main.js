8a902235ab3150f583f69ad7343bc36c7a13521d
Nathan Wong <nathan.wong2002@gmail.com>
2023-06-18 12:50:00 -0600

    Function to create grid complete.

---
+++ sketch/main.js
@@ -0,0 +1,25 @@
+// Global Variables
+const body = document.body;
+const container = document.querySelector(".container");
+const row = document.querySelector(".row");
+const column = document.querySelector(".column");
+
+// Function that creates the divs. Default is 16 x 16.
+// This function will also set the correct CSS layouts.
+function createDivs(x = 4){
+    for (let i = 0; i < (x); i++){
+        const row = document.createElement('div'); 
+        row.classList.add("row");
+        container.appendChild(row);
+
+        for (let j = 0; j < x; j++){
+            const cell = document.createElement('div'); // 'td' = table data.
+            cell.classList.add('cell');
+            cell.innerText = "cell" + j;
+            row.appendChild(cell);
+        }   
+    }
+}
+
+createDivs();
+
