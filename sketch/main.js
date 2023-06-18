c73193a305e0e2e217a780f76b1b9164466d04fd
Nathan Wong <nathan.wong2002@gmail.com>
2023-06-18 12:59:14 -0600

    added grid lines for cells

--- sketch/main.js
+++ sketch/main.js
@@ -6,7 +6,7 @@ const column = document.querySelector(".column");
 
 // Function that creates the divs. Default is 16 x 16.
 // This function will also set the correct CSS layouts.
-function createDivs(x = 4){
+function createDivs(x = 16){
     for (let i = 0; i < (x); i++){
         const row = document.createElement('div'); 
         row.classList.add("row");
@@ -15,7 +15,7 @@ function createDivs(x = 4){
         for (let j = 0; j < x; j++){
             const cell = document.createElement('div'); // 'td' = table data.
             cell.classList.add('cell');
-            cell.innerText = "cell" + j;
+            cell.innerText = "cell";
             row.appendChild(cell);
         }   
     }
