6fbde607494f5d6a71d303a2cbc875db362d397f
Nathan Wong <nathan.wong2002@gmail.com>
2023-06-19 18:06:36 -0600

    reset, and change color options.

--- sketch/main.js
+++ sketch/main.js
@@ -1,6 +1,11 @@
 // Global Variables
 const body = document.body;
 const container = document.querySelector(".container");
+const btn_changeSize = document.querySelector("#btn_changeSize");
+const btn_changeColor = document.querySelector("#btn_changeColor");
+const btn_resetColor = document.querySelector("#btn_resetColor");
+const colorPicker = document.querySelector("#colorPicker");
+const currentColor = document.querySelector("#currentColor");
 
 // Function that creates the divs. Default is 16 x 16.
 // This function will also set the correct CSS layouts.
@@ -15,7 +20,7 @@ function createDivs(x = 16){
             // Give the cell properties.
             cell.classList.add('cell');
             cell.addEventListener('mouseover', () => {
-                cell.setAttribute('style', 'background-color:aqua;');
+                cell.setAttribute('style', 'background-color: ' + colorPicker.value + ';');
             });
             
             //cell.innerText = "cell";
@@ -24,7 +29,7 @@ function createDivs(x = 16){
     }
 }
 
-// Function to re-size the grid upon button click.
+// Button: Resize grid.
 function divSize(){
     size = prompt("Enter size of the grid (0 - 19): ");
 
@@ -41,12 +46,43 @@ function divSize(){
     createDivs(size);
 }
 
+// Button: Change hover color.
+function changeColor(){
+    const allCells = Array.from(document.querySelectorAll('.cell'));
+    colorPicker.click(); // Opens the color picker (default hidden)
+    
+    allCells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.setAttribute('style', 'background-color: ' + colorPicker.value + ';');
+        })
+    });
+    
+}
+
+
+// Button: Reset the grid color to blank.
+function resetColor(){
+    const allCells = Array.from(document.querySelectorAll('.cell'));
+    allCells.forEach((cell) => {
+        cell.setAttribute('style', 'background-color: white');
+    });
+    // for (let i = 0; i < allCells.length; i++){
+    //     allCells[i].setAttribute('style', 'background-color:white');
+    // }
+}
+
 function main(){
+    // Initial state.
     createDivs();
 
-    // Event listeners
-    const btn_changeSize = document.querySelector("#btn_changeSize");
+    // Event listeners.
     btn_changeSize.addEventListener('click', divSize);
+    btn_changeColor.addEventListener('click', changeColor);
+    btn_resetColor.addEventListener('click', resetColor);
+
+    
+
+
 }
 
 main();
