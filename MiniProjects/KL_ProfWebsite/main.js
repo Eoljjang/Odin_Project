6e307e16103410a1222c0df4fb59cb584c73d039
Nathan Wong <nathan.wong2002@gmail.com>
2023-06-26 12:04:57 -0600

    fix #2 for copy

--- MiniProjects/KL_ProfWebsite/main.js
+++ MiniProjects/KL_ProfWebsite/main.js
@@ -8,8 +8,12 @@ function copyToClipboard(icon){
     let phoneNumber = 7804927157;
     let email = 'kloepelm@ualberta.ca';
 
+    // 1) Check if icon is the phone or email.
+    // 2) Check if browser is "inSecureContext". Then you can use the navigator clipboard method.
+    // 3) Otherwise, use the deprecated "document.execCommand" method.
+
     if (icon === "phone"){
-        if (navigator.clipboard){
+        if (navigator.clipboard && window.isSecureContext){
             navigator.clipboard.writeText(phoneNumber)
             // Sucessfully copies to clipboard.
             .then(function(){
@@ -21,10 +25,20 @@ function copyToClipboard(icon){
                 console.error("Failed to copy phone number.");
             })
         }
+        else{
+            var tempInput = document.createElement("input");
+            tempInput.setAttribute("value", phoneNumber);
+            document.body.appendChild(tempInput);
+            tempInput.select();
+            document.execCommand("copy");
+            document.body.removeChild(tempInput);
+            showSnackBar();
+        }
 
     }
     else if (icon === "email"){
-        navigator.clipboard.writeText(email)
+        if (navigator.clipboard && window.isSecureContext){
+            navigator.clipboard.writeText(email)
             .then(function(){
                 showSnackBar();
                 console.log("Email copied to clipboard.");
@@ -32,6 +46,16 @@ function copyToClipboard(icon){
             .catch(function(error){
                 console.error("Failed to copy email.");
             })
+        }
+        else{
+            var tempInput = document.createElement("input");
+            tempInput.setAttribute("value", email);
+            document.body.appendChild(tempInput);
+            tempInput.select();
+            document.execCommand("copy");
+            document.body.removeChild(tempInput);
+            showSnackBar();
+        }
     }
 }
 
