b678147ebc4481b9026eaa550bc156da77f9dee4
Nathan Wong <nathan.wong2002@gmail.com>
2023-07-16 14:27:21 -0600

    structure

---
+++ Projects/library/main.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function book(title, author, length) {
+    this.title = title;
+    this.author = author;
+    this.length = length;
+}
+
+function addBookToLibrary(book){
+    ;
+}
+
+function main(){
+    b1 = book("Monopoly", "MLK", 2000);
+    b2 = book("Stratego", "Steve Jobs", 802);
+}
